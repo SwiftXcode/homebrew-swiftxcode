19b03ca7f4be113f5e94638ff9e8e0e848828b58
Helge Heß <devteam@zeezide.de>
2018-03-02 17:32:36 +0100

    swift-xcode 0.5.2
    Attempt to not call swift-progress when running in brew.

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.1.tar.gz | shasum -a 256
-  sha256 "7030e62b0a76c30af3911ca60ee5efd04ea7f04ac8ebbccd2d272fa3bb0a3512"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.2.tar.gz | shasum -a 256
+  sha256 "625411100c7404f53c68ac618a75d404e298d2a4d2504a75a1099646ac955be1"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
