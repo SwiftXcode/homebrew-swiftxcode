a59c51831d9f95e57b50b690d46f133aa6d2de2a
Helge Heß <devteam@zeezide.de>
2018-03-02 15:35:33 +0100

    Add progress dependency
    ... fully qualified, we'll see.

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -6,6 +6,8 @@ class SwiftXcode < Formula
   # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz | shasum -a 256
   sha256 "052a043615e3872d1f804aa357095caae3d37ff7a1c3929f22655f3bc2060eb4"
   
+  depend_on "swiftxcode/swiftxcode/swift-progress"
+  
   def install
     system "make", "all"
     system "make", "prefix=#{prefix}", "install"
