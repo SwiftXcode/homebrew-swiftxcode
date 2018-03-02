74efc55a85b81ecbbc4ea1deab8f43b7bb308841
Helge Heß <devteam@zeezide.de>
2018-03-02 16:46:26 +0100

    swift-xcode 0.5.1
    Moya + Siesta

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz | shasum -a 256
-  sha256 "052a043615e3872d1f804aa357095caae3d37ff7a1c3929f22655f3bc2060eb4"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.1.tar.gz | shasum -a 256
+  sha256 "7030e62b0a76c30af3911ca60ee5efd04ea7f04ac8ebbccd2d272fa3bb0a3512"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
