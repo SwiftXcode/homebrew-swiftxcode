e69328ea742aebabbbffb1c32ce939c6406482f7
Helge Heß <devteam@zeezide.de>
2018-03-02 15:31:27 +0100

    Swift-xcode formula

---
+++ swift-xcode.rb
@@ -0,0 +1,13 @@
+class SwiftXcode < Formula
+
+  desc "Build Swift Package Manager from within Xcode."
+  homepage "https://swiftxcode.github.io"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz | shasum -a 256
+  sha256 "052a043615e3872d1f804aa357095caae3d37ff7a1c3929f22655f3bc2060eb4"
+  
+  def install
+    system "make", "all"
+    system "make", "prefix=#{prefix}", "install"
+  end
+end
