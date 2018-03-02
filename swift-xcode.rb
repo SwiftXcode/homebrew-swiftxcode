597dcee9d30fd56f9b625f937410a1af4b087bb5
Helge Heß <devteam@zeezide.de>
2018-03-02 15:45:39 +0100

    Depends_on it is

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -6,7 +6,7 @@ class SwiftXcode < Formula
   # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.0.tar.gz | shasum -a 256
   sha256 "052a043615e3872d1f804aa357095caae3d37ff7a1c3929f22655f3bc2060eb4"
   
-  depend_on "swiftxcode/swiftxcode/swift-progress"
+  depends_on "swiftxcode/swiftxcode/swift-progress"
   
   def install
     system "make", "all"
