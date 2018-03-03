6ad5a645719a992c6180c3e6fbeae163f166233e
Helge Heß <devteam@zeezide.de>
2018-03-03 16:47:36 +0100

    swift-xcode 0.5.4

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.3.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.3.tar.gz | shasum -a 256
-  sha256 "714ed0ff56e26ffd568faf70253a14c36ec10bae824fe27e524d546b86e9400e"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.4.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.4.tar.gz | shasum -a 256
+  sha256 "c8530f619dd8a21d5357774fcf3ab8476f6538e8009398a8aff1f5a180c1a35e"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
