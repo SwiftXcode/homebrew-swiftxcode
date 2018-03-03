f27b7ce1b692e191cef76f3a6a406427db5705bd
Helge Hess <devteam@zeezide.de>
2018-03-03 19:37:46 +0100

    swift-xcode-kitura 0.1.3

--- swift-xcode-kitura.rb
+++ swift-xcode-kitura.rb
@@ -2,9 +2,9 @@ class SwiftXcodeKitura < Formula
 
   desc "Build Kitura from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.2.tar.gz"
-  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.2.tar.gz | shasum -a 256
-  sha256 "6dc8161f2f5f63ecb9bdf33f4bb16ce22f886bfd752fb61f5ca30dfe1bc6ad21"
+  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.3.tar.gz"
+  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.3.tar.gz | shasum -a 256
+  sha256 "15ec4d367aee55cfde60d3875e3064cb29a2e80a953144a22f53a5b61a151cd6"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
