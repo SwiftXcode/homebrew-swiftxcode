4ec6c1340260f6f08dff35221ca12814f23f52c0
Helge Heß <devteam@zeezide.de>
2018-03-03 17:04:56 +0100

    Travis: brew install Kitura
    ... see whether it worx.

--- swift-xcode-kitura.rb
+++ swift-xcode-kitura.rb
@@ -2,14 +2,14 @@ class SwiftXcodeKitura < Formula
 
   desc "Build Kitura from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.0.tar.gz | shasum -a 256
-  sha256 "6ec8ff6e929a3ffe3eb64bd741b2d8dbd8ba4c4e6eb3ec785ae437a9a4e00f76"
+  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.1.1.tar.gz | shasum -a 256
+  sha256 "405f41b538e65de1228c3381d596526a3537c02600d1969227ec64a06e0dae9c"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
   def install
-    system "make", "all"
+    # system "make", "all" ... no use
     system "make", "prefix=#{prefix}", "install"
   end
 end
