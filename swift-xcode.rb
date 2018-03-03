c0f8bdc864222f61618a717ebdcb94bc04777f45
Helge Heß <devteam@zeezide.de>
2018-03-03 18:09:46 +0100

    swift-xcode 0.5.5
    ... modmap templates.

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.4.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.4.tar.gz | shasum -a 256
-  sha256 "c8530f619dd8a21d5357774fcf3ab8476f6538e8009398a8aff1f5a180c1a35e"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.5.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.5.tar.gz | shasum -a 256
+  sha256 "6b926a7ca7dcd8ef137d0849233352750b6f372cfaa558c49c647715a383ff79"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
