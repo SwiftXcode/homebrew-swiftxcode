df15660377eb66f4703ea5d87381a686b8e73dc1
Helge Heß <devteam@zeezide.de>
2018-03-03 16:46:54 +0100

    swift-progress 0.9.1

--- swift-progress.rb
+++ swift-progress.rb
@@ -2,9 +2,9 @@ class SwiftProgress < Formula
 
   desc "Visualize Swift Package Manager progress in a window."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-progress/archive/0.9.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-progress/archive/0.9.0.tar.gz | shasum -a 256
-  sha256 "e4e2c94868973104d82ff766ff572e4d600a5a45e9f9692fcdfe39c238ad1787"
+  url "https://github.com/SwiftXcode/swift-progress/archive/0.9.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-progress/archive/0.9.1.tar.gz | shasum -a 256
+  sha256 "b41e467d46db29852ca7d840ea986d4bd76286083cefed8601bb08602d111d41"
   
   def install
     system "make", "all"
