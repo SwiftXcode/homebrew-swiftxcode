36784ff6f09361cdce4d94d45e5d5b8c2a3f80bb
Helge Hess <devteam@zeezide.de>
2018-03-04 20:13:14 +0100

    Depend nio on ressl
    ... not really required for NIO, but for the SSL part.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -6,6 +6,7 @@ class SwiftXcodeNio < Formula
   # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.0.tar.gz | shasum -a 256
   sha256 "6b88a5b64e400ae6c08929f2776dc4ed95c1c59e219153514f10a85353669d5f"
   
+  depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
   def install
