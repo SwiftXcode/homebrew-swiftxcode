816693c27b1cb093b3059ab47739e9d62ede8a1f
Helge Hess <devteam@zeezide.de>
2018-03-05 00:05:37 +0100

    swift-xcode 0.5.11
    makefile fixes

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.10.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.10.tar.gz | shasum -a 256
-  sha256 "2c2561aa7523b18e6fcd59867cd93a10b6d962c6d6a3e85890fd7629b2318098"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.11.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.11.tar.gz | shasum -a 256
+  sha256 "9e0c3f12cfb3f5848510d05dbbba3a49a2ce1cbfa7bbf687f35b61f4fffae2b9"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
