b8a6210f0d73a0c30e88d4abe16800d08c45c01c
Helge Hess <devteam@zeezide.de>
2018-03-04 20:10:05 +0100

    swift-xcode-nio formula

--- swift-xcode-server-all.rb
+++ swift-xcode-server-all.rb
@@ -7,6 +7,7 @@ class SwiftXcodeServerAll < Formula
   # curl -L https://github.com/SwiftXcode/server-all/archive/0.1.0.tar.gz | shasum -a 256
   sha256 "967e38ea75bfdaff7f2c81118ba429d1c5d3ba7ade3ee801b672bb704d88b781"
   
+  depends_on "swiftxcode/swiftxcode/swift-xcode-nio"
   depends_on "swiftxcode/swiftxcode/swift-xcode-server-api"
   depends_on "swiftxcode/swiftxcode/swift-xcode-kitura"
 
