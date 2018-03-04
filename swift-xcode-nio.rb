df9c2ef6246bc02ff795affc7076a00ec75b5cef
Helge Hess <devteam@zeezide.de>
2018-03-04 22:48:57 +0100

    swift-nio 0.1.2
    dooo ...

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.1.tar.gz | shasum -a 256
-  sha256 "f4f085650387c70aba253000d211830ec91cf3f9df823d3be4d2ec66c0f45cf4"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.2.tar.gz | shasum -a 256
+  sha256 "ac74da71e11f9a726e18657c94781fee741bd177808fcd217c54dbddb2f3971e"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
