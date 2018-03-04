b564ec9bc6daa88f75d8dc4f402dc207e62d3edb
Helge Hess <devteam@zeezide.de>
2018-03-04 01:53:03 +0100

    Add URL/sha to server-all
    ... doesn't work w/o.

--- swift-xcode-server-all.rb
+++ swift-xcode-server-all.rb
@@ -3,6 +3,10 @@ class SwiftXcodeServerAll < Formula
   desc "Build Swift servers from within Xcode."
   homepage "https://swiftxcode.github.io"
   
+  url "https://github.com/SwiftXcode/server-all/archive/0.1.0.tar.gz"
+  # curl -L https://github.com/SwiftXcode/server-all/archive/0.1.0.tar.gz | shasum -a 256
+  sha256 "967e38ea75bfdaff7f2c81118ba429d1c5d3ba7ade3ee801b672bb704d88b781"
+  
   depends_on "swiftxcode/swiftxcode/swift-xcode-server-api"
   depends_on "swiftxcode/swiftxcode/swift-xcode-kitura"
 end
