b17dc8fa357326897a96ee176ce7be0bc913b10e
Helge Hess <devteam@zeezide.de>
2018-03-04 01:45:02 +0100

    Attempt to do umbrella formula
    ... will it work?

---
+++ swift-xcode-server-all.rb
@@ -0,0 +1,8 @@
+class SwiftXcodeServerAll < Formula
+
+  desc "Build Swift servers from within Xcode."
+  homepage "https://swiftxcode.github.io"
+  
+  depends_on "swiftxcode/swiftxcode/swift-xcode-server-api"
+  depends_on "swiftxcode/swiftxcode/swift-xcode-kitura"
+end
