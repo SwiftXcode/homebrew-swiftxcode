dc2704d5c546e48ed09a5b3d96e8c2f5b148d222
Helge Hess <devteam@zeezide.de>
2018-03-04 20:26:52 +0100

    nio 0.1.1
    ... fix makefile include issue.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.0.tar.gz | shasum -a 256
-  sha256 "6ad24174b487143fdaee905782547e1b7289cd3e77dbe67996daed239417ffbe"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.1.tar.gz | shasum -a 256
+  sha256 "f4f085650387c70aba253000d211830ec91cf3f9df823d3be4d2ec66c0f45cf4"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
