6566333c1311033fc91591d0216294003d5ad986
Helge Hess <devteam@zeezide.de>
2018-03-04 18:18:40 +0100

    swift-xcode 0.5.9
    ... now installing makefiles ...

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.8.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.8.tar.gz | shasum -a 256
-  sha256 "b675204e2bc6f39f1a1326237314ec194bf4ff26efecf63c0e616477428d5fab"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.5.9.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.5.9.tar.gz | shasum -a 256
+  sha256 "be099f7772f30811ee9254d7781a3ec32ab52a28a7986a7b9c78bd881c2dbc8f"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
