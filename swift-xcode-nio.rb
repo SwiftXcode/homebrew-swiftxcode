be04236a4677cd4d8330ba28cfa0a6b710de6aa5
Helge Hess <devteam@zeezide.de>
2018-03-04 20:23:42 +0100

    Fix SwiftNIO hash

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -4,7 +4,7 @@ class SwiftXcodeNio < Formula
   homepage "https://swiftxcode.github.io"
   url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.0.tar.gz"
   # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.0.tar.gz | shasum -a 256
-  sha256 "6b88a5b64e400ae6c08929f2776dc4ed95c1c59e219153514f10a85353669d5f"
+  sha256 "6ad24174b487143fdaee905782547e1b7289cd3e77dbe67996daed239417ffbe"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
