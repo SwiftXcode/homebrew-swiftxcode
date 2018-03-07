c65733eef8148fc9de50647ce72e18a830946914
Helge Heß <devteam@zeezide.de>
2018-03-07 15:57:26 +0100

    Fix NIO template to use 1.1.0
    -nio v0.1.4

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.3.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.3.tar.gz | shasum -a 256
-  sha256 "9806e04f750adef137c754e90d961791c9450c6cb4f0950c833486edb6799204"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.4.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.4.tar.gz | shasum -a 256
+  sha256 "84df645ac224a8ee4577d1fa1f2a141ab675c727abff052db3ea37e80425c9ae"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
