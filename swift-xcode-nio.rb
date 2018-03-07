94ba1988157314ed5d04b2a5560d45c4712098ff
Helge Hess <devteam@zeezide.de>
2018-03-07 11:01:28 +0100

    Refer to Setty 1.1
    All the great new stuff.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.2.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.2.tar.gz | shasum -a 256
-  sha256 "ac74da71e11f9a726e18657c94781fee741bd177808fcd217c54dbddb2f3971e"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.3.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.1.3.tar.gz | shasum -a 256
+  sha256 "9806e04f750adef137c754e90d961791c9450c6cb4f0950c833486edb6799204"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
