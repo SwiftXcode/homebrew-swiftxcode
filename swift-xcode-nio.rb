effea8e3adf273ca1eb6170a4f982523aa663c05
Helge Hess <devteam@zeezide.de>
2018-03-11 23:35:00 +0100

    Use image 0.2.1, which includes MicroExpress
    ... for your convenience.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.0.tar.gz | shasum -a 256
-  sha256 "129bb3587c5d46689a5ce78555852ec6be6a67508e16a2e38599e4281ebe866e"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.1.tar.gz | shasum -a 256
+  sha256 "c3f8acb6a69a68ddacecfc6802bec3386ea9fc479fcd82848de41f5479ee7581"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
