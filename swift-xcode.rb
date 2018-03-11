41593d7d6a69f7f9788eaf00e0401359f26223b8
Helge Hess <devteam@zeezide.de>
2018-03-11 18:37:27 +0100

    swift-xcode 0.6.1, w/ fix for issue #6
    Do not call ar when there is no input.

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.6.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.6.0.tar.gz | shasum -a 256
-  sha256 "89b214eca8b2269544745122e65604f2552e8767cc8da41e53aece1aca6612fc"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.6.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.6.1.tar.gz | shasum -a 256
+  sha256 "e17b4f14b986d5bfaa562c706c7b5fffaecca55682ef0bea8c0e8eb428a95a66"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
