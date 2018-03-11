f48427f07990a7967596aaa764705262cd1a2462
Helge Hess <devteam@zeezide.de>
2018-03-12 00:08:43 +0100

    Fix syntax error in template
    .. \(

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.1.tar.gz | shasum -a 256
-  sha256 "c3f8acb6a69a68ddacecfc6802bec3386ea9fc479fcd82848de41f5479ee7581"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.2.2.tar.gz | shasum -a 256
+  sha256 "abebfdc9ba3643fd0af9de232e99e8408e8bc559d95f2508657b65806624172c"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
