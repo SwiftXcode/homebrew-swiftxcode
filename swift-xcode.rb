bb5278e731446acc97ad972463379eb627dd01e3
Helge Hess <devteam@zeezide.de>
2018-04-02 12:28:17 +0200

    Fix for issue #8, Xcode 9.3 AppIcon
    Use swift-xcode 0.6.2.

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.6.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.6.1.tar.gz | shasum -a 256
-  sha256 "e17b4f14b986d5bfaa562c706c7b5fffaecca55682ef0bea8c0e8eb428a95a66"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.6.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.6.2.tar.gz | shasum -a 256
+  sha256 "c483e2276594673a305084565d5db7d55dfde4a5a3c75e5d81cd52977893c121"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
