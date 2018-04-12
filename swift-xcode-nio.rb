f9ac16bfce73104c52fae942513bf8ef1e790b73
Helge Heß <devteam@zeezide.de>
2018-04-12 15:31:08 +0200

    NIO Image and template w/ Redis support

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.2.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.2.tar.gz | shasum -a 256
-  sha256 "7983cb3c4c3efb75795e402fcd46b1bf457abeaa129bbebd2e0d8fb97f544662"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.3.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.3.tar.gz | shasum -a 256
+  sha256 "e4f9b4e265c6c1f5e94a8df9d83121529373cc36eb9e033725aa8629b8185cdd"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
