666a8bb0b797518b1e574885cb5571e8a89e8a18
Helge Heß <devteam@zeezide.de>
2018-04-12 15:15:18 +0200

    Use Setty 0.4.2
    ... fixes crashers.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.1.tar.gz | shasum -a 256
-  sha256 "d1e76cb9e133497051e1b03e2233ad13aca22cc1c78ec81c00ee171a47fa1449"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.2.tar.gz | shasum -a 256
+  sha256 "7983cb3c4c3efb75795e402fcd46b1bf457abeaa129bbebd2e0d8fb97f544662"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
