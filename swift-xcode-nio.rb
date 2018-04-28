9dbdfa8f2d592037722964c3d577e2756ea34803
Helge Heß <devteam@zeezide.de>
2018-04-28 13:47:36 +0200

    Use NIO 0.5.0 image w/ boilerplate generation
    Includes some basic boilerplate to spin up new
    servers in a minute.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.4.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.4.4.tar.gz | shasum -a 256
-  sha256 "55c87db0b751c7b113522dcbbfbdbd0ce015c23e9d4ad1644dc45203519aac41"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.0.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.0.tar.gz | shasum -a 256
+  sha256 "e3bfd56ce037e8a35b7baa5c732366e02c32767d1b1ca0fb767922635d406373"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
