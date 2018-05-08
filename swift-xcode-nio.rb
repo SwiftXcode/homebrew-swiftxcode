e938a662aa286ac824001e6985181f1d2acb3a26
Helge Heß <devteam@zeezide.de>
2018-05-08 14:21:13 +0200

    NIO 1.6.1 + IRC image

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.0.tar.gz | shasum -a 256
-  sha256 "e3bfd56ce037e8a35b7baa5c732366e02c32767d1b1ca0fb767922635d406373"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.5.1.tar.gz | shasum -a 256
+  sha256 "8ef66a41b6fcca62eb7ff9a15ecedcf3dfd558d6de2d1b0004564872d062200f"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
