0d04fb73998c5eefbcc25fb9d7cc1f6eb37f1eb1
Helge Heß <devteam@zeezide.de>
2018-06-02 17:08:08 +0200

    Fix typo in Package.swift
    ...

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -2,9 +2,9 @@ class SwiftXcodeWo < Formula
 
   desc "Build Swift WebObjects applications within Xcode."
   homepage "http://swiftobjects.org"
-  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.0.tar.gz"
-  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.0.tar.gz | shasum -a 256
-  sha256 "64ba5f72318bdeec67881bc812c7bbb2e2f4bb46883c59fcca450dd03e31fdb1"
+  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.1.tar.gz"
+  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.1.tar.gz | shasum -a 256
+  sha256 "f98ae95e9df9a813392ebbb56086b75d4e6bcabdb1f3af95becde00fa361b458"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
