ec2ca901cf75dd9f83cf11d3002fcf04da940ede
Helge Heß <devteam@zeezide.de>
2018-06-02 17:02:41 +0200

    Use So 0.5.0

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -2,9 +2,9 @@ class SwiftXcodeWo < Formula
 
   desc "Build Swift WebObjects applications within Xcode."
   homepage "http://swiftobjects.org"
-  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.0.4.tar.gz"
-  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.0.4.tar.gz | shasum -a 256
-  sha256 "bf821cc17b081e46fef079774bda2155f669c8739d0e7f2263f5859dd2e5573d"
+  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.0.tar.gz"
+  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.0.tar.gz | shasum -a 256
+  sha256 "64ba5f72318bdeec67881bc812c7bbb2e2f4bb46883c59fcca450dd03e31fdb1"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
