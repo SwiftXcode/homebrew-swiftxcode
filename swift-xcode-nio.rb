583301ee24d12aa382ca02b31ccc1cb30930cfeb
Helge Heß <me@helgehess.eu>
2018-06-09 11:35:13 +0200

    Use template w/ NIO 1.8.0 API fixes
    ... avoid deprecation warnings.

--- swift-xcode-nio.rb
+++ swift-xcode-nio.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio < Formula
 
   desc "Build Swift IO servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.8.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.8.0.tar.gz | shasum -a 256
-  sha256 "2899606900823b68d80b5cce090186a7e99b7cf89b8d6c7962ae9c2a24d8ddc6"
+  url "https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.8.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO_XcodeImage/archive/0.8.1.tar.gz | shasum -a 256
+  sha256 "90a75648da345d511dad056e05ed1ca6bdfc11fc2655e8ff656a94776a021cf2"
   
   depends_on "libressl"
   depends_on "swiftxcode/swiftxcode/swift-xcode"
