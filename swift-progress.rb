f8e9e9c17c823107a0e858c933cb6361c53e650e
Helge Heß <devteam@zeezide.de>
2018-07-29 15:59:21 +0200

    Use swift-progress 0.9.2 w/ DerivedData fix
    See https://github.com/SwiftXcode/swift-xcode/issues/12

--- swift-progress.rb
+++ swift-progress.rb
@@ -2,9 +2,9 @@ class SwiftProgress < Formula
 
   desc "Visualize Swift Package Manager progress in a window."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-progress/archive/0.9.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-progress/archive/0.9.1.tar.gz | shasum -a 256
-  sha256 "b41e467d46db29852ca7d840ea986d4bd76286083cefed8601bb08602d111d41"
+  url "https://github.com/SwiftXcode/swift-progress/archive/0.9.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-progress/archive/0.9.2.tar.gz | shasum -a 256
+  sha256 "1b2e87c8e4c152db3f3bd7933f907fb6cb7df038efc69b5598f2e5576791b711"
   
   def install
     system "make", "all"
