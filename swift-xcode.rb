7beda69cfb6c75725d5ddbce4f3e4e28c53a8760
Helge Heß <devteam@zeezide.de>
2019-03-16 17:49:46 +0100

    Support for headers in Sources/Pkg/include
    ...

--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.7.3.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.7.3.tar.gz | shasum -a 256
-  sha256 "307e1d33b30e4d7c5284fbe4a38abf2e2591a501d0649d10093defb88f3cc4b9"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.7.4.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.7.4.tar.gz | shasum -a 256
+  sha256 "60e660e161f523e8ffec573a26dfe641c361846ed09ee2ace323b70d648111bc"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
