49bc9701e39aa9ecff0f89d02b9aee0928b6befc
Helge Heß <devteam@zeezide.de>
2019-03-21 17:02:27 +0100

    Kitura 2.6 and swift-xcode 0.7.5
    Updated image & swift-xcode for new Kitura.

--- swift-xcode-kitura.rb
+++ swift-xcode-kitura.rb
@@ -2,9 +2,9 @@ class SwiftXcodeKitura < Formula
 
   desc "Build Kitura from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.2.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.2.0.tar.gz | shasum -a 256
-  sha256 "d6488c592506be00187a3782bf09a7c42b8489d1ca6b59a5c982fe024cbc3a89"
+  url "https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.2.6.tar.gz"
+  # curl -L https://github.com/SwiftXcode/Kitura_XcodeImage/archive/0.2.6.tar.gz | shasum -a 256
+  sha256 "23ca95d76f229f3bc3ce466c8aab7d4248f4e67a14d5acb43c2b79ed5483eacb"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
--- swift-xcode.rb
+++ swift-xcode.rb
@@ -2,9 +2,9 @@ class SwiftXcode < Formula
 
   desc "Build Swift Package Manager from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/swift-xcode/archive/0.7.4.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.7.4.tar.gz | shasum -a 256
-  sha256 "60e660e161f523e8ffec573a26dfe641c361846ed09ee2ace323b70d648111bc"
+  url "https://github.com/SwiftXcode/swift-xcode/archive/0.7.5.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-xcode/archive/0.7.5.tar.gz | shasum -a 256
+  sha256 "d534320b4a0e40bfb88e8d2eeb2e8b7e5a608f41e87da259d7a719a4f8077800"
   
   depends_on "swiftxcode/swiftxcode/swift-progress"
   
