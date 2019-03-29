8dd559d55a0ffd1a8af4c75b403a0c5d4c7202f1
Helge Heß <devteam@zeezide.de>
2019-03-29 18:23:06 +0100

    NIO 2 boilerplate fixes
    ...

--- swift-xcode-nio2.rb
+++ swift-xcode-nio2.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio2 < Formula
 
   desc "Build SwiftNIO 2 servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.3.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.3.tar.gz | shasum -a 256
-  sha256 "a7fb95c78622d3c57132d51f3533a27a016d03a2e27f07a2a8ec2426b490bfbd"
+  url "https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.4.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.4.tar.gz | shasum -a 256
+  sha256 "8bc974406c385b29987fba5d51b936421e84c36952324b9b210f5e8d2c31a256"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
