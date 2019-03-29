233fd34fd68fafc40c61331b987a8819b6249787
Helge Heß <devteam@zeezide.de>
2019-03-29 17:36:46 +0100

    NIO 2 MicroExpress fixes
    Also updated the boilerplate to NIO 2.

--- swift-xcode-nio2.rb
+++ swift-xcode-nio2.rb
@@ -2,9 +2,9 @@ class SwiftXcodeNio2 < Formula
 
   desc "Build SwiftNIO 2 servers from within Xcode."
   homepage "https://swiftxcode.github.io"
-  url "https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.2.tar.gz"
-  # curl -L https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.2.tar.gz | shasum -a 256
-  sha256 "93f43940cf88775309fda35ef806282d2c2a3695e319ac5ae3c984d14b251302"
+  url "https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.3.tar.gz"
+  # curl -L https://github.com/SwiftXcode/SwiftNIO2_XcodeImage/archive/0.10.3.tar.gz | shasum -a 256
+  sha256 "a7fb95c78622d3c57132d51f3533a27a016d03a2e27f07a2a8ec2426b490bfbd"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
