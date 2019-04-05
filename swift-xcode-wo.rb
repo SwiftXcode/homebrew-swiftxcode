64364e563db1a4f3edc756b90d7a3fec8ea75f71
Helge Heß <devteam@zeezide.de>
2019-04-05 17:01:20 +0200

    Update swift-xcode-wo to Swift 5
    Required.

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -2,9 +2,9 @@ class SwiftXcodeWo < Formula
 
   desc "Build Swift WebObjects applications within Xcode."
   homepage "http://swiftobjects.org"
-  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.4.tar.gz"
-  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.5.4.tar.gz | shasum -a 256
-  sha256 "371b5b57f99e97ff58a51b5ae4a405bce05f99c16d28f2295cee1fb1adfe1ece"
+  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.0.tar.gz"
+  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.0.tar.gz | shasum -a 256
+  sha256 "5df8a6571cadb0cc760c760f5ef63e8cb4c2ab813a8d63be2e616d15b2010598"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
