b0a20563078b5a4d3a2bd8bedf355886da074e2e
Helge Heß <devteam@zeezide.de>
2019-04-05 17:52:11 +0200

    Use proper hash for 0.6.1
    ...

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -4,7 +4,7 @@ class SwiftXcodeWo < Formula
   homepage "http://swiftobjects.org"
   url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.1.tar.gz"
   # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.1.tar.gz | shasum -a 256
-  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  sha256 "6bfd1ea84afe2c571f27a6e2404d7ce2711c8dfc5c53968b9acc8d4313d3f08c"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
