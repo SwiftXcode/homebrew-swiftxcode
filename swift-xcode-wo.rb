747e97bb0710c7ce48939d2091a20e302c7324e1
Helge Heß <me@helgehess.eu>
2019-04-10 23:23:41 +0200

    SwiftObjects 0.7.0, which includes ZeeQL
    ...

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -2,9 +2,9 @@ class SwiftXcodeWo < Formula
 
   desc "Build Swift WebObjects applications within Xcode."
   homepage "http://swiftobjects.org"
-  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.1.tar.gz"
-  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.6.1.tar.gz | shasum -a 256
-  sha256 "6bfd1ea84afe2c571f27a6e2404d7ce2711c8dfc5c53968b9acc8d4313d3f08c"
+  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.0.tar.gz"
+  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.0.tar.gz | shasum -a 256
+  sha256 "6b26b9cb49edc743a65c42c4e49f529e4a83c215774c49f710234c1918c1b8cf"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
