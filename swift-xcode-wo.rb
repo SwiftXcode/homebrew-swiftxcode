7e7f2198bd1222b7f21ab198a3ffad26d89c1973
Helge Heß <me@helgehess.eu>
2019-04-11 11:55:01 +0200

    So image w/ ZeeQL bridge
    ...

--- swift-xcode-wo.rb
+++ swift-xcode-wo.rb
@@ -2,9 +2,9 @@ class SwiftXcodeWo < Formula
 
   desc "Build Swift WebObjects applications within Xcode."
   homepage "http://swiftobjects.org"
-  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.1.tar.gz"
-  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.1.tar.gz | shasum -a 256
-  sha256 "558a2d29c1d701808e7fb3bcaff09f5ed38d9b48d0fe458e5f5739b34d6b3705"
+  url "https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.2.tar.gz"
+  # curl -L https://github.com/SwiftObjects/SwiftObjects_XcodeImage/archive/0.7.2.tar.gz | shasum -a 256
+  sha256 "e8d46a0b1d0da935f5505202099736eb60b40c3bc66cb5466c9c397f63b1a9ea"
   
   depends_on "swiftxcode/swiftxcode/swift-xcode"
   
